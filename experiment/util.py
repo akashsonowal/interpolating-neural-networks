d32e5d3538930902ae00628ba11a78787f58bcb5
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-06 21:39:24 +0530

    Update util.py

--- experiment/util.py
+++ experiment/util.py
@@ -1,1 +1,26 @@
+import tensorflow as tf
+from interpolating_neural_network.models import model
 
+loss_object = tf.keras.losses.MeanSquaredError()
+optimizer = tf.keras.optimizers.Adam()
+
+train_loss = tf.keras.metrics.MeanSquaredError(name='train_loss')
+test_loss = tf.keras.metrics.MeanSquaredError(name='test_loss')
+
+@tf.function
+def train_step(features, labels):
+  with tf.GradientTape() as tape:
+    predictions = model(features, training=True)
+    loss = loss_object(labels, predictions)
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+  train_loss(loss)
+  
+@tf.function
+def train_step(features, labels):
+    predictions = model(features, training=False)
+    t_loss = loss_object(labels, predictions)
+    
+    test_loss(t_loss)
+  
