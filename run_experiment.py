b0f19bbe25c3de5867d595f8fe8e45dc4164dc62
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-06 18:49:57 +0530

    Update run_experiment.py

--- run_experiment.py
+++ run_experiment.py
@@ -5,11 +5,72 @@ import tensorflow as tf
 class TrainNN:
   pass
 
+train_ds = tf.data.Dataset.from_tensor_slices(
+    (x_train, y_train)).shuffle(10000).batch(32)
+
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+loss_object = tf.keras.losses()
+optimizer = tf.keras.optimizers.Adam()
+train_metric = tf.keras.metrics.Mean(name="Train Loss")
+test_metric = tf.keras.metrics.Mean(name="Train Loss")
+
+@tf.function
+def train_step(images, labels):
+  with tf.GradientTape() as tape:
+    # training=True is only needed if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    predictions = model(images, training=True)
+    loss = loss_object(labels, predictions)
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+  train_loss(loss)
+  train_accuracy(labels, predictions)
+    
+@tf.function
+def test_step(images, labels):
+  # training=False is only needed if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+  predictions = model(images, training=False)
+  t_loss = loss_object(labels, predictions)
+
+  test_loss(t_loss)
+  test_accuracy(labels, predictions)
+  
+EPOCHS = 5
+
+for epoch in range(EPOCHS):
+  # Reset the metrics at the start of the next epoch
+  train_loss.reset_states()
+  train_accuracy.reset_states()
+  test_loss.reset_states()
+  test_accuracy.reset_states()
+
+  for images, labels in train_ds:
+    train_step(images, labels)
+
+  for test_images, test_labels in test_ds:
+    test_step(test_images, test_labels)
+
+  print(
+    f'Epoch {epoch + 1}, '
+    f'Loss: {train_loss.result()}, '
+    f'Accuracy: {train_accuracy.result() * 100}, '
+    f'Test Loss: {test_loss.result()}, '
+    f'Test Accuracy: {test_accuracy.result() * 100}'
+  )
+  
+  
+
 class NewRiskCurveExperiment:
   def __init__(self):
     pass
   
   def run():
+    
+    
+    
     with tf.GradientTape() as tape:
       
     pass
