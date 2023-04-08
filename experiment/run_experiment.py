4a04e48bb66e2c6e4ba9f57293addf104e931ee5
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-08 09:04:00 +0530

    Update run_experiment.py

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -1,5 +1,7 @@
-import logging
 import argparse
+from pathlib import Path
+
+import logging
 import tensorflow as tf
 
 logger = logging.getLogger(__name__)
@@ -8,6 +10,32 @@ if is_wandb_available():
   import wandb
 
 logger.info('Say Hi')
+
+np.random_seed(42)
+tf.random_set_seed(42)
+
+def _setup_parser():
+  """Setup Python's ArgumentParser with data, model, trainer, and other arguments."""
+  pass
+
+def main():
+  parser = _setup_parser()
+  args = parser.parse_args()
+  data, model = setup_data_and_model_from_args(args)
+  
+  log_dir = Path("training") / "logs"
+  _ensure_logging_dir(log_dir)
+  
+  if args.wandb:
+    logger = 
+    
+  trainer = xyz
+  trainer.fit(model, datamodule)
+
+if __name__ == "__main__":
+  main()
+
+
 
 class TrainNN:
   pass
@@ -21,31 +49,37 @@ loss_object = tf.keras.losses()
 optimizer = tf.keras.optimizers.Adam()
 train_metric = tf.keras.metrics.Mean(name="Train Loss")
 test_metric = tf.keras.metrics.Mean(name="Train Loss")
-
-@tf.function
-def train_step(images, labels):
-  with tf.GradientTape() as tape:
-    # training=True is only needed if there are layers with different
-    # behavior during training versus inference (e.g. Dropout).
-    predictions = model(images, training=True)
-    loss = loss_object(labels, predictions)
-  gradients = tape.gradient(loss, model.trainable_variables)
-  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-  train_loss(loss)
-  train_accuracy(labels, predictions)
-    
-@tf.function
-def test_step(images, labels):
-  # training=False is only needed if there are layers with different
-  # behavior during training versus inference (e.g. Dropout).
-  predictions = model(images, training=False)
-  t_loss = loss_object(labels, predictions)
-
-  test_loss(t_loss)
-  test_accuracy(labels, predictions)
   
 EPOCHS = 5
+
+with mirrored_strategy.scope():
+  model = xyz
+  optimizer = tf.keras.optimizers.Adam()
+
+loss_object = tf.keras.losses.BinaryCrossentropy(
+  from_logits=True,
+  reduction=tf.keras.losses.Reduction.NONE)
+
+def compute_loss(labels, predictions):
+  per_example_loss = loss_object(labels, predictions)
+  return tf.nn.compute_average_loss(per_example_loss, global_batch_size=global_batch_size)
+
+def train_step(inputs):
+  features, labels = inputs
+
+  with tf.GradientTape() as tape:
+    predictions = model(features, training=True)
+    loss = compute_loss(labels, predictions)
+
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+  return loss
+
+@tf.function
+def distributed_train_step(dist_inputs):
+  per_replica_losses = mirrored_strategy.run(train_step, args=(dist_inputs,))
+  return mirrored_strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses,
+                         axis=None)
 
 for epoch in range(EPOCHS):
   # Reset the metrics at the start of the next epoch
