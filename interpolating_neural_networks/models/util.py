938cc4373d6b38fd9ef6c1b513ca767a451072d3
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-09 17:32:48 +0530

    Update util.py

--- interpolating_neural_networks/models/util.py
+++ interpolating_neural_networks/models/util.py
@@ -1,18 +1,5 @@
 from tensorflow.keras.layers import Input, Dense
 
-# class BaseNN(tf.keras.Model):
-#   def __init__(self, input_dim):
-#     super(BaseNN, self).__init__()
-#     self.input_layer = Input(shape=(input_dim, ))
-#     self.hidden_layers = None
-#     self.output_layer = Dense(1, activation='linear')
-  
-#   def call(self, x):
-#     x = self.input_layer(x)
-#     for layer in self.hidden_layers:
-#       x = layer(x)
-#     return self.output_layer(x)
-  
 class BaseMLP(tf.keras.Model):
   def __init__(self):
     super(BaseMLP, self).__init__()
