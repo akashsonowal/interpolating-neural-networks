453b190f2f6fdc712e0ec53f2e41d347de19230e
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-09 17:25:16 +0530

    Update data_loader.py

--- interpolating_neural_networks/data/data_loader.py
+++ interpolating_neural_networks/data/data_loader.py
@@ -20,17 +20,3 @@ class DistributedDataLoader:
     
     def __call__(self):
         return self.train_dataloader, self.val_dataloader
-
-# BUFFER_SIZE = len(x_train)
-# BATCH_SIZE_PER_REPLICA = args.BATCH_SIZE_PER_REPLICA
-# GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
-# EPOCHS = args.EPOCHS
-
-# train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(BUFFER_SIZE).batch(GLOBAL_BATCH_SIZE) 
-# test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(GLOBAL_BATCH_SIZE) 
-# train_dist_dataset = strategy.experimental_distribute_dataset(train_dataset)
-# test_dist_dataset = strategy.experimental_distribute_dataset(test_dataset)
-
-# train_dataloader, val_dataloader =  DistributedDataLoder(train_dataset, val_dataset, 
-#                                                            batch_size=args.BATCH_SIZE_PER_REPLICA, 
-                                                        #    num_workers=strategy.num_replicas_in_sync)
