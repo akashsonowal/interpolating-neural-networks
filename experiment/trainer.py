e27e53d5b8ddeb20b188809a227491cbb9957afd
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-09 18:44:02 +0530

    Update trainer.py

--- experiment/trainer.py
+++ experiment/trainer.py
@@ -48,22 +48,7 @@ class MLPDistributedTrainer:
 
 
 ################3
-with strategy.scope():
-  loss_object = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.NONE)
 
-  def compute_loss(labels, predictions, model_losses):
-    per_example_loss = loss_object(labels, predictions) 
-    loss = tf.nn.compute_average_loss(per_example_loss,
-                                      global_batch_size=GLOBAL_BATCH_SIZE)
-    if model_losses:
-      loss += tf.nn.scale_regularization_loss(tf.add_n(model_losses))
-    return loss
-  
-  train_loss = tf.keras.metrics.MeanSquaredError(name='train_loss')
-  test_loss = tf.keras.metrics.MeanSquaredError(name='test_loss')
-
-  model = model = ExperimentalMLP(args)
-  optimizer = tf.keras.optimizers.Adam()
   
 # logger.info('Building model....')
 
