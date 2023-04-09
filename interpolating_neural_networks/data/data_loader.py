e7b04eeef0ba39b5132621ee5c3b6ac594f30978
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-09 15:14:06 +0530

    update dataloader

--- interpolating_neural_networks/data/data_loader.py
+++ interpolating_neural_networks/data/data_loader.py
@@ -1,18 +1,36 @@
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = args.train_test_split, shuffle=False) 
+import tensorflow as tf
 
 strategy = tf.distribute.MirroredStrategy()
-print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
-BUFFER_SIZE = len(x_train)
-BATCH_SIZE_PER_REPLICA = args.BATCH_SIZE_PER_REPLICA
-GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
-EPOCHS = args.EPOCHS
+class DistributedDataLoader:
+    def __init__(self, train_dataset, val_dataset, batch_size, num_workers):
+        self.global_bs = batch_size * num_workers
+        self.train_dataset = self.tensor_slices(train_dataset, True)
+        self.val_dataset = self.tensor_slices(val_dataset, False)
+        self.train_dataloader = self.distribute_data(self.train_dataset)
+        self.val_dataloader = self.distribute_data(self.val_dataset)
+    
+    def tensor_slices(self, dataset, train):
+        if train:
+            return tf.data.Dataset.from_tensor_slices(dataset).shuffle(len(train_dataset)).batch(self.global_bs)
+        return tf.data.Dataset.from_tensor_slices(dataset).batch(self.global_bs)
+    
+    def distribute_data(self, dataset):
+        return strategy.experimental_distribute_dataset(dataset)
+    
+    def __call__(self):
+        return self.train_dataloader, self.val_dataloader
 
-train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(BUFFER_SIZE).batch(GLOBAL_BATCH_SIZE) 
-test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(GLOBAL_BATCH_SIZE) 
-train_dist_dataset = strategy.experimental_distribute_dataset(train_dataset)
-test_dist_dataset = strategy.experimental_distribute_dataset(test_dataset)
+# BUFFER_SIZE = len(x_train)
+# BATCH_SIZE_PER_REPLICA = args.BATCH_SIZE_PER_REPLICA
+# GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
+# EPOCHS = args.EPOCHS
 
-train_dataloader, val_dataloader =  DistributedDataLoder(train_dataset, val_dataset, 
-                                                           batch_size=args.BATCH_SIZE_PER_REPLICA, 
-                                                           num_workers=strategy.num_replicas_in_sync)
+# train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(BUFFER_SIZE).batch(GLOBAL_BATCH_SIZE) 
+# test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(GLOBAL_BATCH_SIZE) 
+# train_dist_dataset = strategy.experimental_distribute_dataset(train_dataset)
+# test_dist_dataset = strategy.experimental_distribute_dataset(test_dataset)
+
+# train_dataloader, val_dataloader =  DistributedDataLoder(train_dataset, val_dataset, 
+#                                                            batch_size=args.BATCH_SIZE_PER_REPLICA, 
+                                                        #    num_workers=strategy.num_replicas_in_sync)
