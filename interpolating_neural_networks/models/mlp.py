41f8790fb629eb6557f3c6c31f55f8a4ba53d06b
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-09 17:32:04 +0530

    Update mlp.py

--- interpolating_neural_networks/models/mlp.py
+++ interpolating_neural_networks/models/mlp.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
-from .util import BaseNN
-
-class DeepNN(BaseNN):
-  def __init__(self, depth, input_dim):
-    super(DeepNN, self).__init__(input_dim)
-    self.hidden_layers = [Dense(32, activation='relu') for _ in range(depth)]
-  
-class WideNN(BaseNN):
-  def __init__(self, width, input_dim):
-    super(WideNN, self).__init__(input_dim)
-    self.hidden_layers = [Dense(width, activation='relu') for _ in range(2)]
+from .util import BaseMLP
+    
+class ExperimentalMLP(BaseMLP):
+  def __init__(self, **kwargs):
+    super(ExperimentalMLP, self).__init__()
+    if kwargs['depth'] is not None:
+      self.input_layer = Dense(32, input_shape=(kwargs['input_dim'], ))
+      self.hidden_layers = [Dense(32, activation='relu') for _ in range(kwargs['depth']-1)]
+    elif kwargs['width'] is not None: 
+      self.input_layer = Dense(kwargs['width'], input_shape=(kwargs['input_dim'], ))
+      self.hidden_layer = Dense(kwargs['width'], activation='relu')
