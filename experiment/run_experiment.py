121ef0f4f9e9fdb9823600096fb6362d85a0c551
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-09 14:20:49 +0530

    update run_experiment.py

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -13,8 +13,8 @@ tf.random_set_seed(42)
 def get_args_parser():
     parser = argparse.ArgumentParser('Interpolating NN experiment setup')
     parser.add_argument('--train_test_split', default=1/3, type=float, help='train test split ratio')
-    parser.add_argument('--BATCH_SIZE_PER_REPLICA', default=32, type=int, help='batch size of training on a single GPU')
-    parser.add_argument('--EPOCHS', default=15, type=int, help='training epochs')
+    parser.add_argument('--batch_size_per_replica', default=32, type=int, help='batch size of training on a single GPU')
+    parser.add_argument('--epochs', default=15, type=int, help='training epochs')
     parser.add_argument('--input_dim', default=100, type=int, help='input feature set size')
     parser.add_argument('--linear', default=False, const=False, nargs='?', choices=[False, True], help='linear pattern in data')
     parser.add_argument('--expt_type', default='depth', const='depth', nargs='?', choices=['depth', 'width'], help='experiment type')
@@ -29,19 +29,19 @@ def main(args):
 
   dataset = FinancialDataset(data_dir, input_dim=args.input_dim, linear=args.linear)
   train_dataloader, val_dataloader =  DistributedDataLoder(train_dataset, val_dataset, 
-                                                           batch_size=args.BATCH_SIZE_PER_REPLICA, 
+                                                           batch_size=args.batch_size_per_replica, 
                                                            num_workers=strategy.num_replicas_in_sync)
 
   with strategy.scope():
     if args.expt_type=='depth':
       for depth in args.depths:
           model = ExperimentalMLP(input_dim=args.input_dim, depth=depth, width=None)
-          trainer = MLPDistributedTrainer(epochs=EPOCHS, callbacks=[WandbCallBack()])
+          trainer = MLPDistributedTrainer(epochs=args.epochs, callbacks=[WandbCallBack()])
           trainer.fit(model, train_dataloader, val_dataloader)
     else:
       for width in args.widths:
           model = ExperimentalMLP(input_dim=args.input_dim, depth=None, width=width)
-          trainer = MLPDistributedTrainer(epochs=EPOCHS, callbacks=[WandbCallBack()])
+          trainer = MLPDistributedTrainer(epochs=args.epochs, callbacks=[WandbCallBack()])
           trainer.fit(model, train_dataloader, val_dataloader)
 
 if __name__ == '__main__':
