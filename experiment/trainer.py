cebb8baeb7516821928916f27d76b4e8f80f0b05
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-09 19:25:30 +0530

    Update trainer.py

--- experiment/trainer.py
+++ experiment/trainer.py
@@ -3,18 +3,19 @@
 # trainer.fit(model, train_dataloader, val_dataloader)
 import tensorflow as tf
 from .util import strategy, compute_loss, loss_object, train_loss, optimizer
+startegy = 
 
 class MLPDistributedTrainer:
   def __init__(self, model, epochs):
     self.model = model
     self.epochs = epochs 
-  
+    
   @tf.function
   def train_step(self, inputs):
     features, labels = inputs
     with tf.GradientTape() as tape:
       predictions = self.model(features, training=True)
-      loss = compute_loss(features, predictions, model.losses)
+      loss = compute_loss(labels, predictions, model.losses)
 
     gradients = tape.gradient(loss, self.model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
@@ -27,9 +28,8 @@ class MLPDistributedTrainer:
     features, labels = inputs
     predictions = self.model(features, training=False)
     t_loss = loss_object(labels, predictions)
-    test_loss.update_state(labels, predictions)
-    
-    
+    test_loss.update_state(labels, predictions) #(t_loss)
+
   @tf.function
   def distributed_train_step(dataset_inputs):
     per_replica_losses = strategy.run(train_step, args=(dataset_inputs,))
@@ -39,26 +39,24 @@ class MLPDistributedTrainer:
   @tf.function
   def distributed_test_step(dataset_inputs):
     return strategy.run(test_step, args=(dataset_inputs,))
-    
-  @tf.function
-  def distributed_train_epoch(self, dataset):
-    total_loss = 0.0
-    num_batches = 0
-    for x in dataset:
-      per_replica_losses = strategy.run(train_step, args=(x,))
-      total_loss += strategy.reduce(
-        tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
-      num_batches += 1
-    return total_loss / tf.cast(num_batches, dtype=tf.float32)
 
   def fit(self, model, train_dataloader, val_dataloader):
-    for epoch in range(self.epochs):
-      train_loss = self.distributed_train_epoch(train_dist_dataset)
+      with strategy.scope():
+        val_loss = tf.keras.metrics.Mean(name='val_loss')
+      
+      for epoch in range(self.epochs):
+        # Train Loop
+        total_loss = 0.0
+        num_batches = 0
+        for x in train_data_loader:
+          total_loss += self.distributed_train_step(model, x)
+          num_batches += 1
+        train_loss = total_loss / num_batches
+        # Validation Loop
+        for x in val_data_loader:
+          self.distributed_test_step(model, x)
+        val_loss.reset_states()
 
-      template = ("Epoch {}, Loss: {}")
-      print(template.format(epoch + 1, train_loss))
-
-      if callbacks is not None:
-        for callback in callbacks:
-          print(train_loss.numpy())
-          callback.on_epoch_end(epoch, logs={"train_loss": train_loss.numpy()})
+      if self.callbacks is not None:on
+        for callback in self.callbacks:
+          callback.on_train_end(logs={"train_loss": train_loss.numpy(), "val_loss": val_loss.numpy()})
