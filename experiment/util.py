b024c2c38ea3a904b4e5655765af9613386e1a5c
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-10 08:10:16 +0530

    Update util.py

--- experiment/util.py
+++ experiment/util.py
@@ -5,8 +5,7 @@ import tensorflow as tf
 
 class WandbCallBack(tf.keras.callbacks.Callback):
   def __init__(self, args):
-      config_dict = {arg: getattr(args, arg) for arg in vars(args)}
-      wandb.init(project="Interpolating NN Experiments", config=config_dict)
+      wandb.init(project="Interpolating NN Experiments", config=vars(args)
       super(WandbCallBack, self).__init__()
   def on_epoch_end(self, epoch, logs={}):
       wandb.log(logs)
