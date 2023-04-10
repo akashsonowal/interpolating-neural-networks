1e4357cc7b287667eff03c894f59a8c70b9df2ad
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-10 07:47:12 +0530

    Update data_loader.py

--- interpolating_neural_networks/data/data_loader.py
+++ interpolating_neural_networks/data/data_loader.py
@@ -5,14 +5,15 @@ strategy = tf.distribute.MirroredStrategy()
 class DistributedDataLoader:
     def __init__(self, train_dataset, val_dataset, batch_size, num_workers):
         self.global_bs = batch_size * num_workers
-        self.train_dataset = self.tensor_slices(train_dataset, True)
-        self.val_dataset = self.tensor_slices(val_dataset, False)
+        self.buffer = len(train_dataset)
+        self.train_dataset = self.tensor_slices(train_dataset, train=True)
+        self.val_dataset = self.tensor_slices(val_dataset, train=False)
         self.train_dataloader = self.distribute_data(self.train_dataset)
         self.val_dataloader = self.distribute_data(self.val_dataset)
     
     def tensor_slices(self, dataset, train):
         if train:
-            return tf.data.Dataset.from_tensor_slices(dataset).shuffle(len(self.train_dataset)).batch(self.global_bs)
+            return tf.data.Dataset.from_tensor_slices(dataset).shuffle(self.buffer).batch(self.global_bs)
         return tf.data.Dataset.from_tensor_slices(dataset).batch(self.global_bs)
     
     def distribute_data(self, dataset):
