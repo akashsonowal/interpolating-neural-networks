62859eaace20d923f41e0acbcef0d5682e2b7fbf
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-11 08:18:55 +0530

    Update util.py

--- interpolating_neural_networks/models/util.py
+++ interpolating_neural_networks/models/util.py
@@ -2,17 +2,20 @@ import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense
 
 class BaseMLP(tf.keras.Model):
-  def __init__(self, **kwargs):
+  def __init__(self, strategy, **kwargs):
     super(BaseMLP, self).__init__()
-    self.depth = kwargs['depth']
-    self.width = kwargs['width']
-    self.output_layer = Dense(1, activation='linear')
+    self.strategy = strategy
+    with self.strategy.scope():
+      self.depth = kwargs['depth']
+      self.width = kwargs['width']
+      self.output_layer = Dense(1, activation='linear')
   
   def call(self, x):
-    x = self.input_layer(x)
-    if self.depth:
-      for layer in self.hidden_layers:
-        x = layer(x)
-    elif self.width:
-      x = self.hidden_layer(x)
-    return self.output_layer(x)
+    with self.strategy.scope():
+      x = self.input_layer(x)
+      if self.depth:
+        for layer in self.hidden_layers:
+          x = layer(x)
+      elif self.width:
+        x = self.hidden_layer(x)
+      return self.output_layer(x)
