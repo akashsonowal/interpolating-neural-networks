93817ae262579063c9b1452ac14d1b2861d04244
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-11 09:32:41 +0530

    Update mlp.py

--- interpolating_neural_networks/models/mlp.py
+++ interpolating_neural_networks/models/mlp.py
@@ -5,14 +5,11 @@ from .util import BaseMLP
 # model = ExperimentalMLP(input_dim=args.input_dim, depth=depth, width=None)
     
 class ExperimentalMLP(BaseMLP):
-  def __init__(self, strategy, **kwargs):
-    super(ExperimentalMLP, self).__init__(strategy, **kwargs)
-    self.strategy = strategy
-    
-    with self.strategy.scope():
-        if kwargs['depth'] is not None:
-          self.input_layer = Dense(32, input_shape=(kwargs['input_dim'], ))
-          self.hidden_layers = [Dense(32, activation='relu') for _ in range(kwargs['depth']-1)]
-        elif kwargs['width'] is not None: 
-          self.input_layer = Dense(kwargs['width'], input_shape=(kwargs['input_dim'], ))
-          self.hidden_layer = Dense(kwargs['width'], activation='relu')
+  def __init__(self, **kwargs):
+    super(ExperimentalMLP, self).__init__(**kwargs)
+    if kwargs['depth'] is not None:
+      self.input_layer = Dense(32, input_shape=(kwargs['input_dim'], ))
+      self.hidden_layers = [Dense(32, activation='relu') for _ in range(kwargs['depth']-1)]
+    elif kwargs['width'] is not None: 
+      self.input_layer = Dense(kwargs['width'], input_shape=(kwargs['input_dim'], ))
+      self.hidden_layer = Dense(kwargs['width'], activation='relu')
