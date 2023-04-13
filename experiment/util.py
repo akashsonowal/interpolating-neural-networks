d98cfeec5beed22fc4e66794136a3e7bfd991475
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-13 21:30:28 +0530

    Update util.py

--- experiment/util.py
+++ experiment/util.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # coding=utf-8
-# import wandb
+import wandb
 import tensorflow as tf
 
 class WandbCallBack(tf.keras.callbacks.Callback):
   def __init__(self):
       super(WandbCallBack, self).__init__()
   def on_train_end(self, logs={}):
-      print(logs)
-#       wandb.log(logs)
+      wandb.log(logs)
