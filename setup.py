6da583a331ba0d5653a127cd3d9da32316f95b95
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-13 21:50:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
+from setuptools import setup, find_packages
 
+setup(
+  name = 'interpolating-neural-networks',
+  packages = find_packages(exclude=['examples']),
+  version = '0.0.1',
+  license = 'MIT',
+  
