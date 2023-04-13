d26fd759d534a20b696af16ddb81c792fedf76a1
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-13 21:54:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
   packages = find_packages(exclude=['examples']),
   version = '0.0.1',
   license = 'MIT',
+  author = 'Akash Sonowal',
+  author_email = 'work.akashsonowal@gmail.com',
+  url = 'https://github.com/akashsonowal/interpolating-neural-networks/'
+)
   
