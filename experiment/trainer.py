128685689e43462716d9e957c32bab031b3fade1
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-13 20:40:51 +0530

    Update trainer.py

--- experiment/trainer.py
+++ experiment/trainer.py
@@ -13,8 +13,9 @@ class MLPDistributedTrainer:
       self.train_loss_metric = tf.keras.metrics.MeanSquaredError(name='train_loss_metric')
       self.val_loss_metric = tf.keras.metrics.MeanSquaredError(name='val_loss_metric')
   
-  with strategy.scope():
-    def _compute_loss(self, labels, predictions, model_losses, global_batch_size):
+
+  def _compute_loss(self, labels, predictions, model_losses, global_batch_size):
+    with self.strategy.scope():
       per_example_loss = self.loss_object(labels, predictions)
       loss = tf.nn.compute_average_loss(per_example_loss,
                                         global_batch_size=global_batch_size)
