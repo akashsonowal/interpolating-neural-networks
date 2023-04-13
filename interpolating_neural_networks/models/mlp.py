189c17701f5ae013725b1447220a2bab2c3060c4
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-13 20:19:36 +0530

    Update mlp.py

--- interpolating_neural_networks/models/mlp.py
+++ interpolating_neural_networks/models/mlp.py
@@ -7,9 +7,9 @@ from .util import BaseMLP
 class ExperimentalMLP(BaseMLP):
   def __init__(self, **kwargs):
     super(ExperimentalMLP, self).__init__(**kwargs)
-    if kwargs['depth'] is not None:
+    if kwargs['depth']:
       self.input_layer = Dense(32, input_shape=(kwargs['input_dim'], ))
       self.hidden_layers = [Dense(32, activation='relu') for _ in range(kwargs['depth']-1)]
-    elif kwargs['width'] is not None: 
+    elif kwargs['width']: 
       self.input_layer = Dense(kwargs['width'], input_shape=(kwargs['input_dim'], ))
       self.hidden_layer = Dense(kwargs['width'], activation='relu')
