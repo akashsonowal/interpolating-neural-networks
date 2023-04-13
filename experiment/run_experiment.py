f1bd20c49a425c4e02fc015dab1252aef2582a10
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-13 07:30:52 +0530

    Update run_experiment.py

--- experiment/run_experiment.py
+++ experiment/run_experiment.py
@@ -5,8 +5,8 @@ import random
 import numpy as np
 import tensorflow as tf
 
-from experiment.util import WandbCallBack
-from experiment.trainer import MLPDistributedTrainer
+from .util import WandbCallBack
+from .trainer import MLPDistributedTrainer
 from interpolating_neural_networks.data import FinancialDataset, DistributedDataLoader
 from interpolating_neural_networks.models import ExperimentalMLP
 
