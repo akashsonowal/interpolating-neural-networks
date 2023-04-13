34047ed2b6965bfe2a2ce7c3c8d21ba714ddcdd6
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-13 20:12:40 +0530

    Update data_loader.py

--- interpolating_neural_networks/data/data_loader.py
+++ interpolating_neural_networks/data/data_loader.py
@@ -5,17 +5,17 @@ class DistributedDataLoader:
         self.strategy = strategy
         self.global_bs = batch_size * num_workers
         self.buffer = len(train_dataset)
-        self.train_dataset = self.tensor_slices(train_dataset, train=True)
-        self.val_dataset = self.tensor_slices(val_dataset, train=False)
-        self.train_dataloader = self.distribute_data(self.train_dataset)
-        self.val_dataloader = self.distribute_data(self.val_dataset)
+        self.train_dataset = self._tensor_slices(train_dataset, train=True)
+        self.val_dataset = self._tensor_slices(val_dataset, train=False)
+        self.train_dataloader = self._distribute_data(self.train_dataset)
+        self.val_dataloader = self._distribute_data(self.val_dataset)
     
-    def tensor_slices(self, dataset, train):
+    def _tensor_slices(self, dataset, train):
         if train:
             return tf.data.Dataset.from_tensor_slices(dataset).shuffle(self.buffer).batch(self.global_bs)
         return tf.data.Dataset.from_tensor_slices(dataset).batch(self.global_bs)
     
-    def distribute_data(self, dataset):
+    def _distribute_data(self, dataset):
         return self.strategy.experimental_distribute_dataset(dataset)
     
     def __call__(self):
