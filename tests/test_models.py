eae797bd071ab46d9e5bf10bc655311e102c5010
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-15 12:37:40 +0530

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,2 +1,13 @@
 import pytest
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from interpolating.models import ExperimentalMLP
 
+@pytest.fixture
+def mlp():
+  return ExperimentalMLP(input_dim=100, depth=2, width=None)
+
+def test_mlp_output_shape(mlp):
+  x = tf.ones((100, mlp.input_dim))
+  y = mlp(x)
+  assert y.shape == (100, 1)
