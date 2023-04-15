b0c1e8c21f76e5714c90ab471739c47aa36caa81
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-15 14:02:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
   keywords = ["double descent", "deep learning", "generalization", "asset pricing"],
   python_requires=">=3.8",
   install_requires = [required_packages],
-  extra_requires = {"dev": style_packages + ["pre-commit==2.19.0"], 
+  extra_requires = {"dev": style_packages + test_packages + ["pre-commit==2.19.0"], 
                     "test": test_packages},
   classifiers = [
           "Development Status :: 4 - Beta",
