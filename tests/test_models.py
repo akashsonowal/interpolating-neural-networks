dd500c28972da846ec90456f2024907701111362
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-15 16:24:02 +0530

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,12 +1,15 @@
 import pytest
 import tensorflow as tf
+
 from interpolating_neural_networks.models import ExperimentalMLP
+
 
 @pytest.fixture
 def mlp():
-  return ExperimentalMLP(input_dim=100, depth=2, width=None)
+    return ExperimentalMLP(input_dim=100, depth=2, width=None)
+
 
 def test_mlp_output_shape(mlp):
-  x = tf.ones((100, 100))
-  y = mlp(x)
-  assert y.shape == (100, 1)
+    x = tf.ones((100, 100))
+    y = mlp(x)
+    assert y.shape == (100, 1)
