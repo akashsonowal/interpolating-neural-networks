25f8d42be2a8d6e4db732f45c712585656502d24
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-15 12:24:57 +0530

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,22 +6,28 @@ from interpolating_neural_networks.data import FinancialDataset, DistributedData
 
 def test_folder_contains_files():
     assert all(Path('data').joinpath(file_name).is_file() for file_name in ['c_50.csv', 'r2_50.csv'])
+
+def test_dataset():
+    dataset = FinancialDataset(Path('data'), train_val_split=1/3, input_dim=50, linear=False)
+    assert isinstance(dataset, FinancialDataset)
     
-@pytest.fixture
-def strategy():
-    return tf.distribute.OneDeviceStrategy(device='/cpu:0')
+# @pytest.fixture
+# def strategy():
+#     return tf.distribute.OneDeviceStrategy(device='/cpu:0')
 
-train_dataset, val_dataset = FinancialDataset(Path('data'), train_val_split=1/3, input_dim=50, linear=False)()
 
-@pytest.fixture
-def dataloader(strategy, train_dataset, val_dataset):
-    return DistributedDataLoader(strategy, train_dataset, val_dataset, batch_size=32, num_workers=1)
 
-def test_dataloader_shape(dataloader):
-    train_dataloader, val_dataloader = dataloader()
-    for x in train_dataloader:
-        assert x[0].shape == (32, 100)
-        assert x[1].shape == (32, 1)
-    for x in val_dataloader:
-        assert x[0].shape == (32, 100)
-        assert x[1].shape == (32, 1)
+
+
+# @pytest.fixture
+# def dataloader(strategy, train_dataset, val_dataset):
+#     return DistributedDataLoader(strategy, train_dataset, val_dataset, batch_size=32, num_workers=1)
+
+# def test_dataloader_shape(dataloader):
+#     train_dataloader, val_dataloader = dataloader()
+#     for x in train_dataloader:
+#         assert x[0].shape == (32, 100)
+#         assert x[1].shape == (32, 1)
+#     for x in val_dataloader:
+#         assert x[0].shape == (32, 100)
+#         assert x[1].shape == (32, 1)
