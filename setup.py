918f1d8f92868a0362a59375a7dcddcb08281376
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-15 21:29:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ BASE_DIR = Path(__file__).parent
 with open(Path(BASE_DIR, "requirements.txt")) as file:
     required_packages = [line.strip() for line in file.readlines()]
 
+def read_version(fname="experiment/version.py"):
+    exec(compile(open(fname, encoding="utf-8").read(), fname, "exec"))
+    return locals()["__version__"]
+
 style_packages = ["black==22.3.0", "flake8==3.9.2", "isort==5.10.1"]
 test_packages = ["pytest==7.1.2"]
 
