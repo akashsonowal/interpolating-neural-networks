ca8cdfd4f375f70d63aa8d41afd5d52219588f61
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-15 16:18:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 # Load packages from requirements.txt
 BASE_DIR = Path(__file__).parent
@@ -10,25 +11,25 @@ style_packages = ["black==22.3.0", "flake8==3.9.2", "isort==5.10.1"]
 test_packages = ["pytest==7.1.2"]
 
 setup(
-  name = 'interpolating-neural-networks',
-  packages = find_packages(),
-  version = '0.0.1',
-  license = 'MIT',
-  author = 'Akash Sonowal',
-  author_email = 'work.akashsonowal@gmail.com',
-  url = 'https://github.com/akashsonowal/interpolating-neural-networks/',
-  keywords = ["double descent", "deep learning", "generalization", "asset pricing"],
-  python_requires=">=3.8",
-  install_requires = [required_packages],
-  extras_require = {
-      "dev": test_packages + style_packages + ["pre-commit==2.19.0"],
-      "test": test_packages
-  },
-  classifiers = [
-          "Development Status :: 4 - Beta",
-          "Intended Audience :: Science/Research",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python :: 3.8",
-          "Topic :: Science/Engineering :: Artificial Intelligence",            
-  ],
+    name="interpolating-neural-networks",
+    packages=find_packages(),
+    version="0.0.1",
+    license="MIT",
+    author="Akash Sonowal",
+    author_email="work.akashsonowal@gmail.com",
+    url="https://github.com/akashsonowal/interpolating-neural-networks/",
+    keywords=["double descent", "deep learning", "generalization", "asset pricing"],
+    python_requires=">=3.8",
+    install_requires=[required_packages],
+    extras_require={
+        "dev": test_packages + style_packages + ["pre-commit==2.19.0"],
+        "test": test_packages,
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Science/Engineering :: Artificial Intelligence",
+    ],
 )
