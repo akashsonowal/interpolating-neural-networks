585163b1e1d676a0b0f67b52cf1319f4e0ce51c6
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-15 12:39:45 +0530

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,6 +8,6 @@ def mlp():
   return ExperimentalMLP(input_dim=100, depth=2, width=None)
 
 def test_mlp_output_shape(mlp):
-  x = tf.ones((100, mlp.input_dim))
+  x = tf.ones((100, 100))
   y = mlp(x)
   assert y.shape == (100, 1)
