4cf745a2cbeff66fe6c66fb98e1062863b3dee25
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-15 08:18:26 +0530

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,2 +1,26 @@
 import pytest
+import tensorflow as tf
+from interpolating_neural_networks.data import DistributedDataLoader
 
+@pytest.fixture
+def strategy():
+    return tf.distribute.OneDeviceStrategy(device='/cpu:0')
+
+@pytest.fixture
+def train_dataset():
+    return tf.data.Dataset.from_tensor_slices(tf.random.uniform((1000, 10)))
+
+@pytest.fixture
+def val_dataset():
+    return tf.data.Dataset.from_tensor_slices(tf.random.uniform((100, 10)))
+
+@pytest.fixture
+def dataloader(strategy, train_dataset, val_dataset):
+    return DistributedDataLoader(strategy, train_dataset, val_dataset, batch_size=32, num_workers=4)
+
+def test_dataloader_shape(dataloader):
+    train_dataloader, val_dataloader = dataloader()
+    for x in train_dataloader:
+        assert x.shape == (32 * 4, 10)
+    for x in val_dataloader:
+        assert x.shape == (32 * 4, 10)
