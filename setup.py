586146b0f9bdd6ad12fafcd8da3f0a86b675518f
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-04-15 13:47:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import Path
 from setuptools import setup, find_packages
+
+# Load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt")) as file:
+    required_packages = [line.strip() for line in file.readlines()]
 
 style_packages = ["black==22.3.0", "flake8==3.9.2", "isort==5.10.1"]
 test_packages = ["pytest==7.1.2"]
@@ -12,9 +18,9 @@ setup(
   author_email = 'work.akashsonowal@gmail.com',
   url = 'https://github.com/akashsonowal/interpolating-neural-networks/',
   keywords = ["double descent", "deep learning", "generalization", "asset pricing"],
-  install_requires = [
-  ],
-  extra_requires = {"dev": style_packages + ["pre-commit==2.19.0"], "test": test_packages},
+  install_requires = [required_packages],
+  extra_requires = {"dev": style_packages + ["pre-commit==2.19.0"], 
+                    "test": test_packages},
   classifiers = [
           "Development Status :: 4 - Beta",
           "Intended Audience :: Science/Research",
